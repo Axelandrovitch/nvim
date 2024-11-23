82c2766f9e16b2d711e1da7a2a481aefdfab046b
Axelandrovitch <axel.a.hetru@gmail.com>
2024-11-24 00:36:38 +0100

    add basic keymaps

--- init.lua
+++ init.lua
@@ -11,4 +11,19 @@ vim.api.nvim_set_keymap("n", "y", '"+y', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "yy", '"+yy', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "y", '"+y', { noremap = true, silent = true })
 
+
+function Map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
+-- Center cursor 
+Map("n", "<C-d>", "<C-d>zz")
+Map("n", "<C-u>", "<C-u>zz")
+Map("n", "n", "nzzzv")
+Map("n", "N", "Nzzzv")
+
 vim.opt.guicursor = "n-v-i-c:block-Cursor"
