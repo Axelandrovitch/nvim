3a56da81c0db5dd75487b05217532a74d6e9b3ba
Axelandrovitch <axel.a.hetru@gmail.com>
2025-03-30 18:42:59 +0200

    add commenting plugin; remove copilot plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -101,10 +101,17 @@ require("lazy").setup({
 	},
 
 	-- Autres plugins
-	{
+	--[[ {
 		"github/copilot.vim",
 		config = function()
 			-- Configuration optionnelle
 		end
-	}
+	}, ]]
+-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+	{
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    }
+}
 })
